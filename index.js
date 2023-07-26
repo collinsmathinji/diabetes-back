e766b39ea85c02580e2f186fe777f1ad32591077
collinsmathinji <125723113+collinsmathinji@users.noreply.github.com>
2023-07-26 09:04:18 +0300

    main

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://frontend-diabetes.vercel.app"
+  origin: "https://frontend-diabetes-git-main-collinsmathinji.vercel.app"
 }));
 
 app.use((req, res, next) => {
