cf70be55bdd66e947cc60abafd5ed0a0ca7b8e4b
collinsmathinji <125723113+collinsmathinji@users.noreply.github.com>
2023-07-27 08:26:33 +0300

    password strength

--- models/userModel.js
+++ models/userModel.js
@@ -27,7 +27,7 @@ userSchema.statics.signup = async function(email, password) {
     throw Error('Email not valid')
   }
   if (!validator.isStrongPassword(password)) {
-    throw Error('Password not strong enough')
+    throw Error('Use special letter,Numbers,Small & capital letters')
   }
 
   const exists = await this.findOne({ email })
